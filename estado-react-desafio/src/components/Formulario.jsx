7876e806de8377ff673a6458a4f70ca9c46c7b4f
MiguelRamos <miguelramostapia@gmail.com>
2023-12-18 01:29:29 -0600

    desafio pregunta 4

--- estado-react-desafio/src/components/Formulario.jsx
+++ estado-react-desafio/src/components/Formulario.jsx
@@ -6,17 +6,42 @@ function Formulario() {
     const [email, setEmail] = useState('');
     const [contraseña,setContraseña] = useState('');
     const [confirmaContraseña,setConfirmaContraseña] = useState('');
-
+    const [error, setError] = useState(10);
+ 
     const handleNombreChange = (event)=>{setNombre(event.target.value);};
     const handleEmailChange = (event)=>{setEmail(event.target.value);};
     const handleContraseñaChange = (event)=>{setContraseña(event.target.value);};
     const handleConfirmaContraseñaChange = (event)=>{setConfirmaContraseña(event.target.value);};
 
+    const validarInput = (e) => {
+        // Prevenimos el comportamiento por defecto
+        e.preventDefault();
 
+        const regexParacorreo = /.+@.+\.[A-Za-z]+$/;
+
+        if(nombre ===''|| email ==='' || contraseña ==='' || confirmaContraseña===''){setError(0);return;}
+
+        if(email ===''){setError(2);return;}
+
+        if(!regexParacorreo.test(email)){setError(1);return;}
+
+        if(contraseña!=confirmaContraseña){setError(3);return;}
+
+        /*        // Validación input
+        if(nombre==='' || apellido==='' || edad==='' || email==='') {setError(true);return;};
+        setError(false);
+        setNombre('');setApellido('');setEdad('');setEmail('');
+
+*/
+    }
   
   return (
     <>
-        <form >
+        <form className="formulario" onSubmit={validarInput}>
+            {error==0 ? <p className='error'>Todos los campos deben ser ingresados</p> : null}
+            {error==1 ? <p className='error'>El correo está remal escrito pos oiga</p> : null}
+            {error==2 ? <p className='error'>Debe ingresar un correo</p> : null}
+            {error==3 ? <p className='error'>Las Contraseñas deben de ser igualitas</p> : null}
             <div className="form-group">
                 <input type="text" name="nombre" className="form-control" onChange={handleNombreChange} placeholder='Ingrese Nombre'/>
             </div>
